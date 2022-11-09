0b01d0b7082b8650eab77202f752b3143bda01ba
Emil Lind <emil.lind@learnet.se>
2022-11-09 09:17:33 +0100

    updates when getting all products and posting one product

--- fixxo-backend/Models/ProductRequest.cs
+++ fixxo-backend/Models/ProductRequest.cs
@@ -5,6 +5,7 @@
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string Description { get; set; }
+        public ProductColorRequest Color { get; set; }
         public int CategoryId { get; set; }
     }
 }
